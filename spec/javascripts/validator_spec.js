d173d2d4d966ad458369715ac45de1702e58507b
Alex Devero <deveroalex@gmail.com>
2016-11-04 16:22:19 +0100

    Fix houndci offences-double quotes

--- spec/javascripts/validator_spec.js
+++ spec/javascripts/validator_spec.js
@@ -1,16 +1,16 @@
-describe("ICO validator", function() {
+describe('ICO validator', function() {
   var icoList, i;
   icoList = ['61499609', '25275500', '29233011'];
   for( i in icoList ) if( icoList.hasOwnProperty(i) ) {
     var icoValid = icoList[i];
-    it("ICO " + icoValid + " is valid", function() {
+    it('ICO ' + icoValid + ' is valid', function() {
       expect(icoValidator.validIco(icoValid)).toBeTruthy();
     });
   }
   icoList = [null, '', '1', 123, '1111111X', '00000000', '614996097', '123456789'];
   for( i in icoList ) if( icoList.hasOwnProperty(i) ) {
     ico_invalid = icoList[i];
-    it("ICO is invalid with value " + ico_invalid, function() {
+    it('ICO is invalid with value ' + ico_invalid, function() {
       expect(icoValidator.validIco(ico_invalid)).toBeFalsy();
     });
   }
