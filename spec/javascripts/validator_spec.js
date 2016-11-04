665483d91c12b262476d748f3812571a30033f31
Alex Devero <deveroalex@gmail.com>
2016-11-04 15:58:03 +0100

    Modify test to use new name convention

--- spec/javascripts/validator_spec.js
+++ spec/javascripts/validator_spec.js
@@ -4,15 +4,14 @@ describe("ICO validator", function() {
   for( i in ico_list ) if( ico_list.hasOwnProperty(i) ) {
     ico_valid = ico_list[i];
     it("ICO " + ico_valid + " is valid", function() {
-      expect(ico_validator.valid_ico(ico_valid)).toBeTruthy();
+      expect(icoValidator.validIco(ico_valid)).toBeTruthy();
     });
   }
   ico_list = [null, '', '1', 123, '1111111X', '00000000', '614996097', '123456789'];
   for( i in ico_list ) if( ico_list.hasOwnProperty(i) ) {
     ico_invalid = ico_list[i];
     it("ICO is invalid with value " + ico_invalid, function() {
-      expect(ico_validator.valid_ico(ico_invalid)).toBeFalsy();
+      expect(icoValidator.validIco(ico_invalid)).toBeFalsy();
     });
   }
 });
-
