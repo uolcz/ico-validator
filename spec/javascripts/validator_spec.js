b9a411a0a766432b92b5102a7e48ceae7228851e
Alex Devero <deveroalex@gmail.com>
2016-11-04 16:27:10 +0100

    Fix houndci offences-fix if statement

--- spec/javascripts/validator_spec.js
+++ spec/javascripts/validator_spec.js
@@ -1,11 +1,13 @@
 describe('ICO validator', function() {
   var icoList, i;
   icoList = ['61499609', '25275500', '29233011'];
-  for( i in icoList ) if( icoList.hasOwnProperty(i) ) {
-    var icoValid = icoList[i];
-    it('ICO ' + icoValid + ' is valid', function() {
-      expect(icoValidator.validIco(icoValid)).toBeTruthy();
-    });
+  for( i in icoList ) {
+    if( icoList.hasOwnProperty(i) ) {
+      var icoValid = icoList[i];
+      it('ICO ' + icoValid + ' is valid', function() {
+        expect(icoValidator.validIco(icoValid)).toBeTruthy();
+      });
+    }
   }
   icoList = [null, '', '1', 123, '1111111X', '00000000', '614996097', '123456789'];
   for( i in icoList ) {
